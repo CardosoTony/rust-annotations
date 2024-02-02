c1430e300f164257e89e76550373745554c88f72
Zeno <eng.tonyalc@gmail.com>
2024-02-01 23:03:49 -0300

    :recycle: Updating the usage mode of the functions

--- code_examples/simple_types/src/main.rs
+++ code_examples/simple_types/src/main.rs
@@ -1,9 +1,17 @@
+// Mode 1
+/*
 use func_examples::ding;
 use func_examples::on_off;
 use func_examples::print_array;
 use func_examples::print_difference;
 use func_examples::print_distance1;
 use func_examples::print_distance2;
+*/
+
+// Mode 2
+use func_examples::{
+    ding, on_off, print_array, print_difference, print_distance1, print_distance2,
+};
 
 fn main() {
     let coords: (f32, f32) = (6.3, 15.0);
